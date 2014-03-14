2d821dc182e3c8f9847ba62f466428c2e5ddefd0
Wupi Ocktavia K <wupiocktavia@ymail.com>
2014-03-14 18:12:14 +0700

    Perubahan 1
    Menambahkan biodata wupi

---
+++ coba.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Community v8.32 
+MySQL - 5.6.13 : Database - coba
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`coba` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `coba`;
+
+/*Table structure for table `biodata` */
+
+DROP TABLE IF EXISTS `biodata`;
+
+CREATE TABLE `biodata` (
+  `NIM` int(9) NOT NULL,
+  `NAMA_LENGKAP` varchar(40) DEFAULT NULL,
+  `KELAS` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`NIM`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `biodata` */
+
+insert  into `biodata`(`NIM`,`NAMA_LENGKAP`,`KELAS`) values (10111068,'Wupi Ocktavia K','IF-3');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
